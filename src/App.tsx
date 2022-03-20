ad1cf603458170cfb965cc39309b903e8fda764e
Daniel Stefan <stefan.danniell@gmail.com>
2022-03-20 10:07:05 +0200

    Voting card initial setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React, { FunctionComponent } from 'react';
+import { FunctionComponent } from 'react';
 
 export const App: FunctionComponent = (): JSX.Element => {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+    </h1>
     </div>
   );
 }
